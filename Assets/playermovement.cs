0ab813b00c46f24b73eae324c05d0a9bb76b66d8
Cassie Hui <cch542@nyu.edu>
2020-09-27 12:14:52 -0400

    game manager/score not working; pick up and drop not working;

---
+++ Assets/playermovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    public Rigidbody2D thisRigidbody2D;
+    public float speed = 7f;
+
+    Vector2 moveDirection;
+
+
+   void Update()
+    {
+        moveDirection.x = Input.GetAxisRaw("Horizontal");
+        moveDirection.y = Input.GetAxisRaw("Vertical"); 
+    }
+
+    void FixedUpdate()
+    {
+        thisRigidbody2D.MovePosition(thisRigidbody2D.position + moveDirection * speed * Time.fixedDeltaTime);
+    }
+
+    /*
+    void ProcessInputs()
+    {
+
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+
+        ProcessInputs();
+    }
+
+
+    void Move()
+    {
+       thisRigidbody2D.velocity = new Vector2(moveDirection.x * speed, moveDirection.y * speed);
+
+        Move();
+    }
+    */
+}
+
---
+++ Assets/trigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trigger : MonoBehaviour
+{
+    private GameMaster gm;
+
+   void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("gamemaster").GetComponent<GameMaster>();
+       
+     
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("stuffed animal"))
+        {
+            gm.points += 1;
+
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        
+    }
+}
